4ecfbacdaa77fdd2ec1b78e0422cbafbcd334145
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-14 23:12:24 -0700

    Updated References, Info, and Framework
    Updated Project References, Application Info, and Target Framework

--- Server/My Project/AssemblyInfo.vb
+++ Server/My Project/AssemblyInfo.vb
@@ -9,10 +9,10 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Vexis Server")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyDescription("This program will be an information services system.")> 
+<Assembly: AssemblyCompany("Versidyne LLC")> 
 <Assembly: AssemblyProduct("Vexis Server")> 
-<Assembly: AssemblyCopyright("Copyright © Microsoft 2010")> 
+<Assembly: AssemblyCopyright("Copyright © Versidyne LLC 2010-2013")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("0.5.0.0")> 
+<Assembly: AssemblyFileVersion("0.5.0.0")> 
