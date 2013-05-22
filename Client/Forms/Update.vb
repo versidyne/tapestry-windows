ff0d13714b7256b6723a9a5749704ea43ada7309
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-22 00:14:34 -0700

    Updated Update Form Code

--- Client/Forms/Update.vb
+++ Client/Forms/Update.vb
@@ -1,16 +1,18 @@
 ﻿Public Class Update
 
     Private Sub Update_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Downloads update.xml
-        Dim myClient As New Net.WebClient()
-        Dim url As New Uri("http://api.versidyne.com/?session=12345&update=vexis-windows")
-        Dim file As String = My.Application.Info.DirectoryPath & "\Patch\updater.xml"
 
-        AddHandler myClient.DownloadProgressChanged, AddressOf DownloadProgressCallback
+        Dim APIClient As New Net.WebClient()
+        Dim URL As New Uri("http://api.versidyne.com/?session=12345&update=vexis-windows")
+        Dim file As String = My.Application.Info.DirectoryPath & "\Patch\update.xml"
 
-        AddHandler myClient.DownloadFileCompleted, AddressOf DownloadFileCallBack2
+        AddHandler APIClient.DownloadProgressChanged, AddressOf DownloadProgressCallback
+        AddHandler APIClient.DownloadFileCompleted, AddressOf DownloadFileCallBack2
 
-        myClient.DownloadFileAsync(url, file)
+        APIClient.DownloadFileAsync(URL, file)
+
     End Sub
+
+
 
 End Class
