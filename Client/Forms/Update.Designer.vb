48c679a89a0a0f0f75e650dbc3bf64421ca28b68
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-22 00:13:47 -0700

    Updated Form Design
    Added Labels, Buttons, Progress Bar, and Status Bar

--- Client/Forms/Update.Designer.vb
+++ Client/Forms/Update.Designer.vb
@@ -22,16 +22,142 @@ Partial Class Update
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1Output = New System.Windows.Forms.Label()
+        Me.Label2Output = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel1Output = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 165)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(460, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Current Version:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Latest Version:"
+        '
+        'Label1Output
+        '
+        Me.Label1Output.AutoSize = True
+        Me.Label1Output.Location = New System.Drawing.Point(100, 10)
+        Me.Label1Output.Name = "Label1Output"
+        Me.Label1Output.Size = New System.Drawing.Size(13, 13)
+        Me.Label1Output.TabIndex = 2
+        Me.Label1Output.Text = "0"
+        '
+        'Label2Output
+        '
+        Me.Label2Output.AutoSize = True
+        Me.Label2Output.Location = New System.Drawing.Point(100, 35)
+        Me.Label2Output.Name = "Label2Output"
+        Me.Label2Output.Size = New System.Drawing.Size(13, 13)
+        Me.Label2Output.TabIndex = 2
+        Me.Label2Output.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 60)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Check for Update"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 95)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(101, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Download Update"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 130)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(101, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Install Update"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel1Output})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 205)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(484, 22)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(88, 17)
+        Me.ToolStripStatusLabel1.Text = "Current Action:"
+        '
+        'ToolStripStatusLabel1Output
+        '
+        Me.ToolStripStatusLabel1Output.Name = "ToolStripStatusLabel1Output"
+        Me.ToolStripStatusLabel1Output.Size = New System.Drawing.Size(36, 17)
+        Me.ToolStripStatusLabel1Output.Text = "None"
         '
         'Update
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(484, 227)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2Output)
+        Me.Controls.Add(Me.Label1Output)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
         Me.Name = "Update"
-        Me.Text = "Form1"
+        Me.Text = "Update"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1Output As System.Windows.Forms.Label
+    Friend WithEvents Label2Output As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel1Output As System.Windows.Forms.ToolStripStatusLabel
 End Class
