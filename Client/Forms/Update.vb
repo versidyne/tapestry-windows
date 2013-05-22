42e8f0a2bbbd58e1f2f35eab689a41f9a73e77ea
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-21 18:08:48 -0700

    Added Update Form
    Added Update Form for Vexis

---
+++ Client/Forms/Update.vb
@@ -0,0 +1,16 @@
+﻿Public Class Update
+
+    Private Sub Update_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Downloads update.xml
+        Dim myClient As New Net.WebClient()
+        Dim url As New Uri("http://api.versidyne.com/?session=12345&update=vexis-windows")
+        Dim file As String = My.Application.Info.DirectoryPath & "\Patch\updater.xml"
+
+        AddHandler myClient.DownloadProgressChanged, AddressOf DownloadProgressCallback
+
+        AddHandler myClient.DownloadFileCompleted, AddressOf DownloadFileCallBack2
+
+        myClient.DownloadFileAsync(url, file)
+    End Sub
+
+End Class
